a8a417ceb0eddc2d3b7fb1e16a9cd9c6a331bebf
JCBInventory <jcb.inventory@rajeshmotors.com>
2025-11-26 15:13:10 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -4,22 +4,7 @@ import App from './App'
 
 const rootElement = document.getElementById('root')
 
-if (!rootElement) {
-  console.error('Root element not found!')
-  document.body.innerHTML = '<div style="color: red; padding: 20px;">ERROR: Root element not found</div>'
-} else {
-  try {
-    const root = ReactDOM.createRoot(rootElement)
-    root.render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-    )
-  } catch (error) {
-    console.error('React render error:', error)
-    rootElement.innerHTML = `<div style="color: red; padding: 20px; background: #172554; font-family: monospace;">
-      <h2>React Error:</h2>
-      <pre>${error}</pre>
-    </div>`
-  }
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(<App />)
 }
